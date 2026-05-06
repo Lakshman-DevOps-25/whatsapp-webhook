ecd4264973e86eafeda56df2089272f76f3857e7
Lakshman-DevOps-25 <laxmanaraog@gmail.com>
2026-05-06 15:50:49 +0530

    Created index.js file

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = "test123";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+  return res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Webhook:", req.body);
+  res.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 5000);
