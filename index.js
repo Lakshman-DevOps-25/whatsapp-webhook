0ba51da785bb3ff3b2eb523854a7bb96b3f33007
Lakshman-DevOps-25 <laxmanaraog@gmail.com>
2026-05-08 19:08:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,18 @@ mongoose.connect(process.env.MONGO_URI)
 // MINIO
 // ============================================
 
+// const minioClient = new Minio.Client({
+//   endPoint: process.env.MINIO_ENDPOINT,
+//   port: Number(process.env.MINIO_PORT),
+//   useSSL: process.env.MINIO_USE_SSL === "true",
+//   accessKey: process.env.MINIO_ACCESS_KEY,
+//   secretKey: process.env.MINIO_SECRET_KEY
+// });
+
 const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT,
   port: Number(process.env.MINIO_PORT),
-  useSSL: process.env.MINIO_USE_SSL === "true",
+  useSSL: true,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY
 });
